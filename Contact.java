d964aae190e3c3f20accbd27100814947b8bfeb7
hapkvn <huyhoangpkvn75@gmail.com>
2025-12-08 15:34:03 +0700

    cap nhat tạo kết nối với sql

--- Contact.java
+++ Contact.java
@@ -1,3 +1,11 @@
 public class Contact{
 
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/quanLyNothing", "root", "123456");
+    }
+
+    class read{
+
+    }
 }
